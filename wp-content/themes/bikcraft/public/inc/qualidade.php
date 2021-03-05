0295916b1db76a513d29c95043f663c93e8689e6
Gustavo Dias <gustavo.diias1@gmail.com>
2021-03-05 01:32:47 -0300

    Custom fields adicionados

--- wp-content/themes/bikcraft/public/inc/qualidade.php
+++ wp-content/themes/bikcraft/public/inc/qualidade.php
@@ -1,25 +1,21 @@
+<?php $sobre = get_page_by_title('sobre'); ?>
 <section class="qualidade container">
-            <h2 class="subtitulo">Qualidade</h2>
-            <img src="<?php echo get_template_directory_uri(); ?>/public/img/bikcraft-qualidade.png" alt="Bikcraft">
+            <h2 class="subtitulo"><?php the_field('titulo_qualidade', $sobre); ?></h2>
+            <img src="<?php the_field('logo_bikcraft', $sobre); ?>" alt="Bikcraft">
             <ul class="qualidade_lista">
+                <?php if(have_rows('item_qualidade', $sobre)): while(have_rows('item_qualidade', $sobre)) : the_row(); ?>
                 <li class="grid-1-3">
-                    <h3>Durabilidade</h3>
-                    <p>Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
+                    <h3><?php the_sub_field('titulo_item_qualidade', $sobre) ?></h3>
+                    <p><?php the_sub_field('descricao_item_qualidade', $sobre); ?></p>
                 </li>
+                <?php endwhile; else: endif; ?>
 
-                <li class="grid-1-3">
-                    <h3>Design</h3>
-                    <p>Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
-                </li>
-
-                <li class="grid-1-3">
-                    <h3>Sustentabilidade</h3>
-                    <p>Ainda assim, existem dúvidas a respeito de como a necessidade de renovação</p>
-                </li>
             </ul>
 
+            <?php if (!is_page('sobre')) { ?>
             <div class="call">
-                <p>Conheça mais a nossa história</p>
-                <a href="sobre.html" class="btn btn-preto">Sobre</a>
+                <p><?php the_field('chamada_sobre', $sobre) ?></p>
+                <a href="/bikcraftwp/sobre/" class="btn btn-preto">Sobre</a>
             </div>
+            <?php } ?>
         </section>
