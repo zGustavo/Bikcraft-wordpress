17bf3be9011d700d97b9d0b5e44739d6b370b261
Gustavo Dias <gustavo.diias1@gmail.com>
2021-03-06 03:39:08 -0300

    adicionado novos fields, campo portfolio

--- wp-content/themes/bikcraft/page-home.php
+++ wp-content/themes/bikcraft/page-home.php
@@ -55,11 +55,6 @@ get_header();
             <div class="container">
                 <h2 class="subtitulo">Portfólio</h2>
                 <?php include(TEMPLATEPATH . "/public/inc/clientes-portfolio.php"); ?>
-
-                <div class="call">
-                    <p><?php the_field('chamada_portfolio'); ?> </p>
-                    <a href="/bikcraftwp/portfolio/" class="btn">Portfólio</a>
-                </div>
             </div>
         </section>
 
--- wp-content/themes/bikcraft/public/inc/clientes-portfolio.php
+++ wp-content/themes/bikcraft/public/inc/clientes-portfolio.php
@@ -1,13 +1,20 @@
+<?php $portfolio = get_page_by_title('portfolio'); ?>
+
 <ul class="portfolio_lista rslides_portfolio">
+    <?php if(have_rows('item_portfolio', $portfolio)) : while (have_rows('item_portfolio', $portfolio)) : the_row(); ?>
     <li>
-        <div class="grid-8"><img src="<?php echo get_stylesheet_directory_uri(); ?>/public/img/portfolio/retro.jpg" alt="Bicicleta Retro"></div>
-        <div class="grid-8"><img src="<?php echo get_stylesheet_directory_uri(); ?>/public/img/portfolio/passeio.jpg" alt="Bicicleta passeio"></div>
-        <div class="grid-16"><img src="<?php echo get_stylesheet_directory_uri(); ?>/public/img/portfolio/esporte.jpg" alt="Bicicleta esporte"></div>
+        <div class="grid-8"><img src="<?php the_sub_field('portfolio_imagem1', $portfolio); ?>" alt="<?php the_sub_field('portfolio_imagem_descricao1', $portfolio); ?>"></div>
+        <div class="grid-8"><img src="<?php the_sub_field('portfolio_imagem2', $portfolio); ?>" alt="<?php the_sub_field('portfolio_imagem_descricao2', $portfolio); ?>"></div>
+        <div class="grid-16"><img src="<?php the_sub_field('portfolio_imagem3', $portfolio); ?>" alt="<?php the_sub_field('portfolio_imagem_descricao3', $portfolio); ?>"></div>
     </li>
 
-    <li>
-        <div class="grid-8"><img src="<?php echo get_stylesheet_directory_uri(); ?>/public/img/portfolio/passeio.jpg" alt="Bicicleta passeio"></div>
-        <div class="grid-8"><img src="<?php echo get_stylesheet_directory_uri(); ?>/public/img/portfolio/retro.jpg" alt="Bicicleta Retro"></div>
-        <div class="grid-16"><img src="<?php echo get_stylesheet_directory_uri(); ?>/public/img/portfolio/esporte.jpg" alt="Bicicleta esporte"></div>
-    </li>
+    <?php endwhile; else : endif; ?>
+
 </ul>
+
+<?php if (!is_page('portfolio')) { ?>
+    <div class="call">
+        <p><?php the_field('chamada_portfolio', $portfolio) ?></p>
+        <a href="/bikcraftwp/portfolio/" class="btn">Portfólio</a>
+    </div>
+<?php } ?>
