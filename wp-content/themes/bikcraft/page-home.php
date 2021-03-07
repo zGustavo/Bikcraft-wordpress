c9d48b3cb98315328d2de08133f904b1576129c2
Gustavo Dias <gustavo.diias1@gmail.com>
2021-03-07 09:03:14 -0300

    Add imagens responsivas home

--- wp-content/themes/bikcraft/page-home.php
+++ wp-content/themes/bikcraft/page-home.php
@@ -4,6 +4,27 @@ get_header();
 ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<?php 
+    $imagem_id = get_field('background_home');
+    $background_large = wp_get_attachment_image_src($imagem_id, 'large');
+    $background_medium = wp_get_attachment_image_src($imagem_id, 'medium');
+?>
+
+<style type="text/css">
+    .introducao {
+        background: url('<?php echo $background_large[0] ?>') no-repeat center;
+        background-size: cover;
+    }
+
+    @media only screen and (max-width: 767px) {
+        .introducao {
+             background: url('<?php echo $background_medium[0] ?>') no-repeat center;
+             background-size: cover;
+        }
+    }
+</style>
+
         <section class="introducao">
             <div class="container">
                 <h1> <?php the_field('titulo_introducao'); ?> </h1>
