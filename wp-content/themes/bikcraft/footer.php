5c7422733ca9d567441019c24cb3d7b7d79a7c02
Gustavo Dias <gustavo.diias1@gmail.com>
2021-03-10 01:03:44 -0300

    Add custom field footer/contato

--- wp-content/themes/bikcraft/footer.php
+++ wp-content/themes/bikcraft/footer.php
@@ -1,7 +1,16 @@
+<?php $contato = get_page_by_title('contato'); ?>
+
+<style type="text/css">
+    .quebra {
+        background: url("<?php the_field('background_footer', $contato); ?>") no-repeat center;
+        background-size: cover;
+    }
+</style>
+
 <div class="quebra">
     <blockquote class="quote-externo container">
-        <p>O verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida contidiana.</p>
-        <cite>WILLIAM MORRIS</cite>
+        <?php the_field('frase_footer', $contato); ?>
+        <cite><?php the_field('autor_footer', $contato); ?></cite>
     </blockquote>
 </div>
 
@@ -10,17 +19,15 @@
         <div class="container">
             <div class="grid-8 footer_historia">
                 <h3>Nossa história</h3>
-                <p>O verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida contidiana
-                    interesse por todos os detalhes da vida contidiana
-                </p>
+                <?php the_field('resumo_historia', $contato); ?>
             </div>
 
             <div class="grid-4 footer_contato">
                 <h3>Contato</h3>
                 <ul>
-                    <li>- 21 232232-4343</li>
-                    <li>- contato@bikcraft.com</li>
-                    <li>- Botafogo - RJ</li>
+                    <li><?php the_field('telefone', $contato); ?></li>
+                    <li><?php the_field('email', $contato); ?></li>
+                    <li><?php the_field('endereco1', $contato); ?></li>
                 </ul>
             </div>
 
@@ -35,7 +42,7 @@
 
     <div class="copy">
         <div class="container">
-            <p class="grid-16">Bikcraft 2015 - Alguns os direitos reservados</p>
+            <p class="grid-16"><?php bloginfo('name'); ?> <?php echo date('Y'); ?> - Alguns os direitos reservados</p>
         </div>
     </div>
 
