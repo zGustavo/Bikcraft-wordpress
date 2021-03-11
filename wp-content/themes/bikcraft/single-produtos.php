459110e2a938aa84de1b817f297d6987ffb8ad4f
Gustavo Dias <gustavo.diias1@gmail.com>
2021-03-10 23:41:53 -0300

    Single Post Produtos criado

---
+++ wp-content/themes/bikcraft/single-produtos.php
@@ -0,0 +1,65 @@
+<?php
+    // Template Name: Single Produtos
+    get_header();
+?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<section class="container produto_item animar-interno">
+    <div class="grid-11">
+        <img src="<?php the_field('foto_produto1'); ?>" alt="Bikcraft <?php the_title(); ?>">
+        <h2><?php the_title(); ?></h2>
+    </div>
+    <div class="grid-5 produto_icone">
+        <img src="<?php the_field('icone_produto'); ?>" alt="Ícone <?php the_title(); ?>">
+    </div>
+    <div class="grid-8">
+        <img src="<?php the_field('foto_produto2'); ?>" alt="Bikcraft <?php the_title(); ?>">
+    </div>
+    <div class="grid-8 produto_info">
+        <?php the_content(); ?>
+    </div>
+</section>
+
+<section class="orcamento">
+    <div class="container">
+        <h2 class="subtitulo">Orçamento</h2>
+        <form action="enviar.php" method="post" name="form" class="formphp form grid-8">
+            <label for="nome">Nome</label>
+            <input id="nome" name="nome" type="text">
+            <label for="email">E-mail</label>
+            <input id="email" name="email" type="text">
+            <label for="telefone">Telefone</label>
+            <input id="telefone" name="telefone" type="text">
+
+            <label class="nao-aparece">Se você não é um robô, deixe em branco.</label>
+            <input type="text" class="nao-aparece" name="leaveblank">
+            <label class="nao-aparece">Se você não é um robô, não mude este campo.</label>
+            <input type="text" class="nao-aparece" name="dontchange" value="http://">
+
+            <label for="mensagem">Especificações</label>
+            <textarea name="mensagem" id="mensagem"></textarea>
+
+            <button id="enviar" name="enviar" type="submit" class="btn">Enviar</button>
+        </form>
+
+        <div class="orcamento_dados grid-8">
+            <h3>Dados</h3>
+            <span>+55 21 93223-3232</span>
+            <span>orcamento@bikcraft.com</span>
+            <h3>Monte sua Bikcraft</h3>
+            <p>Escolha as especificações</p>
+            <ul>
+                <li>-Cores</li>
+                <li>-Estilo</li>
+                <li>-Medidas</li>
+                <li>-Acessórios</li>
+                <li>E Outros</li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+
+<?php endwhile; else: endif; ?>
+<?php get_footer(); ?>
