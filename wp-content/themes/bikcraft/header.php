a200556216d7c9a91b4b1a1aaaf20541cec7b0f1
Gustavo Dias <gustavo.diias1@gmail.com>
2021-03-18 23:21:26 -0300

    tags SEO

--- wp-content/themes/bikcraft/header.php
+++ wp-content/themes/bikcraft/header.php
@@ -3,18 +3,16 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> <?php bloginfo('name'); ?> </title>
-        <meta name="description" content="Compre sua Bicicleta Personalizada na Bikcraft.
-        Possuímos modelos Passeio, Retrô e Esporte">
+        <title> <?php bloginfo('name'); ?> <?php wp_title(' - '); ?> <?php the_field('title_seo'); ?> </title>
+        <meta name="description" content="<?php bloginfo('name'); ?> <?php wp_title(' - '); ?> <?php the_field('description_seo'); ?>">
 
         <meta property="og:type" content="website">
-        <meta property="og:title" content="<?php bloginfo('name'); ?>">
-        <meta property="og:description" content="Compre sua Bicicleta Personalizada na Bikcraft.
-        Possuímos modelos Passeio, Retrô e Esporte">
-        <meta property="og:url" content="http://bikcraft.com">
-        <meta property="og:image" content="http://bikcraft.com/img/op-image.png">
+        <meta property="og:title" content="<?php bloginfo('name'); ?> <?php wp_title(' - '); ?> <?php the_field('title_seo'); ?>">
+        <meta property="og:description" content="<?php bloginfo('name'); ?> <?php wp_title(' - '); ?> <?php the_field('description_seo'); ?>">
+        <meta property="og:url" content="<?php bloginfo('url'); ?>">
+        <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/op-image.png">
 
-        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
         
         <!--Inicio Wordpress Header-->
         <?php wp_head(); ?>
